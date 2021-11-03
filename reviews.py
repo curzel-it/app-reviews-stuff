ba4f1700371723e24efdfded4fe3d69d80f08825
Federico Curzel <federico.curzel@gucci.com>
2021-11-03 01:24:07 +0100

    Fixed take-it-easy mode

--- reviews.py
+++ reviews.py
@@ -3,14 +3,13 @@ import json
 import time
 import random
 
-def get_reviews_page(auth_token, app_id, locale, platforms, offset=0, per_page=1):
+def get_reviews_page(auth_token, app_id, locale, platforms, offset=0):
   print('[{0}] Getting reviews, offset: {1}'.format(app_id, offset))
-  url = 'https://amp-api.apps.apple.com/v1/catalog/us/apps/{0}/reviews?l={1}&offset={2}&platform=web&additionalPlatforms={3}&sparseLimit={4}'
+  url = 'https://amp-api.apps.apple.com/v1/catalog/us/apps/{0}/reviews?l={1}&offset={2}&platform=web&additionalPlatforms={3}&sparseLimit=10'
   url = url.format(
     app_id.strip(), 
     locale, offset, 
-    ','.join(platforms).replace(' ', ''), 
-    per_page
+    ','.join(platforms).replace(' ', '')
   )
   response = requests.get(
     url, headers={'authorization': 'Bearer {0}'.format(auth_token)}
@@ -25,7 +24,6 @@ def _is_valid_review(data):
 
 def get_all_reviews(app_id, args):
   print('[{0}] Getting all reviews...'.format(app_id))
-  per_page = 50
   offset = 0
   reviews = []
   
@@ -35,13 +33,15 @@ def get_all_reviews(app_id, args):
       app_id, 
       args.locale, 
       args.platforms, 
-      offset, per_page
+      offset
     )
     offset += len(new_reviews)
     reviews += new_reviews
     if len(new_reviews) == 0: break
     if len(reviews) >= args.max_reviews: break
-    delay = float(random.randint(100, 500)) / 1000
+    
+    min_delay, max_delay = (500, 1500) if args.take_it_easy else (100, 500)
+    delay = float(random.randint(min_delay, max_delay)) / 1000
     time.sleep(delay)
 
   return reviews[:min(len(reviews), args.max_reviews)]
